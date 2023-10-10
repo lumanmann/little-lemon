ea94412c25b886d9bab55db6b21463091a9344fd
natalie <nwy0206@gmail.com>
2023-10-09 17:53:50 -0700

    fix: logic of the fake API

--- src/fakeAPI.js
+++ src/fakeAPI.js
@@ -21,7 +21,7 @@ const fetchAPI = (date) => {
 // This function accepts the booking form data as a parameter and will return true if the data was successfully submitted.
 const submitAPI = (formData) => {
   for (let i in formData) {
-    if (i == '' || i == undefined) {
+    if (formData[i]== '' || formData[i] == undefined) {
       return false;
     }
   }
